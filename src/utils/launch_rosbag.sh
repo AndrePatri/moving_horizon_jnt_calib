70d7cc63fa3005c6a1544007ed006bc5d8639526
andrea patrizi <andreapatrizi1b6e6@gmail.com>
2023-04-18 10:40:10 +0200

    fixed typo in bag topic name

--- src/utils/launch_rosbag.sh
+++ src/utils/launch_rosbag.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-rosbag record /xbotcore/aux /xbotcore/command /xbotcore/fault /xbotcore/joint_states /xbotcore/statistics /traj_replayer/calib_traj_status /traj_replayer/jnt_calib_status
+rosbag record /xbotcore/aux /xbotcore/command /xbotcore/fault /xbotcore/joint_states /xbotcore/statistics /traj_replayer/calib_traj_status /mhe/jnt_calib_status
