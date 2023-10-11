1c664ae4c0a192fc74ccb72fde260f5ab5ef085e
Andrea Patrizi <andreapatrizi1b6e6@gmail.com>
2023-10-11 16:47:51 +0200

    added license headers

--- src/calib_traj_replayer_rt.cpp
+++ src/calib_traj_replayer_rt.cpp
@@ -1,3 +1,20 @@
+// Copyright (C) 2023  Andrea Patrizi (AndrePatri, andreapatrizi1b6e6@gmail.com)
+// 
+// This file is part of moving_horizon_jnt_calib and distributed under the General Public License version 2 license.
+// 
+// moving_horizon_jnt_calib is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+// 
+// moving_horizon_jnt_calib is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with moving_horizon_jnt_calib.  If not, see <http://www.gnu.org/licenses/>.
+// 
 #include "calib_traj_replayer_rt.h"
 
 #include <math.h> 
